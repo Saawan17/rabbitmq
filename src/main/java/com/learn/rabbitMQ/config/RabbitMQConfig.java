3c3a5dae71ad661e3b053c8789e4406b8a575b26
Saawan k <saawank17@gmail.com>
2026-01-17 12:43:46 +0530

    adding jackson convertor

--- src/main/java/com/learn/rabbitMQ/config/RabbitMQConfig.java
+++ src/main/java/com/learn/rabbitMQ/config/RabbitMQConfig.java
@@ -1,5 +1,9 @@
 package com.learn.rabbitMQ.config;
 
+import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.context.annotation.Configuration;
 
 import org.springframework.amqp.core.*;
@@ -30,5 +34,35 @@ public class RabbitMQConfig {
                 .to(orderExchange())
                 .with(ROUTING_KEY);
     }
+
+    @Bean
+    public JacksonJsonMessageConverter messageConverter() {
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         JacksonJsonMessageConverter converter) {
+        RabbitTemplate template = new RabbitTemplate(connectionFactory);
+        template.setMessageConverter(converter);
+        return template;
+    }
+
+    @Bean
+    public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(
+            ConnectionFactory connectionFactory,
+            JacksonJsonMessageConverter converter) {
+
+        SimpleRabbitListenerContainerFactory factory =
+                new SimpleRabbitListenerContainerFactory();
+
+        factory.setConnectionFactory(connectionFactory);
+        factory.setMessageConverter(converter);
+
+        factory.setDefaultRequeueRejected(false);
+
+        return factory;
+    }
+
 }
 
